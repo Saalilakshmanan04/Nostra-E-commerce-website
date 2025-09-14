cb7059ce0af4fcc032e4ff4b5904af87eb40a360
Saalilakshmanan04 <saalilakshmanan04@gmail.com>
2025-09-14 14:37:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//banner close
+var banner=document.getElementById("banner");
+var close=document.getElementById("close");
+close.addEventListener("click",function(){
+    banner.remove();
+})
+//Selecting Side navbar,Menu icon
+var sidenav=document.getElementById("sidenav");
+var menuicon=document.getElementById("menuicon");
+var closenav=document.getElementById("closenav");
+menuicon.addEventListener("click",function(){
+    sidenav.style.left=0;
+})
+closenav.addEventListener("click",function(){
+    sidenav.style.left="-50%";
+})
+
+//slider
+let currentSlideID=0; //Index-based(0,1,2)
+
+function showSlide(){
+    const slider=document.getElementById("slider");
+    const slides=slider.getElementsByTagName("li");
+    const slideWidth=slides[0].clientWidth;
+
+    slider.style.transform=`translateX(-${currentSlideID * slideWidth}px)`;
+}
+
+function next(){
+    const slider=document.getElementById("slider");
+    const slides=slider.getElementsByTagName("li");
+
+    currentSlideID=(currentSlideID +1) % slides.length;
+    showSlide();
+}
+function prev(){
+    const slider=document.getElementById("slider");
+    const slides=slider.getElementsByTagName("li");
+
+    currentSlideID = (currentSlideID - 1 + slides.length) % slides.length;
+    showSlide();
+}
+document.addEventListener("DOMContentLoaded",function(){
+    showSlide();
+});
+
+window.next=next;
+window.prev=prev;
